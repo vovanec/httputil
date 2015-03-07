b66fb4c314051314b76c36179acd7a2384cad9fa
Vovan Kuznetsov <vkuznet@juniper.net>
2015-03-07 10:59:40 -0800

    Added license

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ setup(
     packages=['httputil'],
     version='0.1',
     description='Utilities to deal with HTTP data',
+    license='Public Domain',
     author='Vovan Kuznetsov',
     author_email='vovanec@gmail.com',
     url='https://github.com/vovanec/httputil',
     download_url='https://github.com/vovanec/httputil/tarball/0.1',
-    keywords=['http', 'chunk', 'compress'],
+    keywords=['http', 'dechunk', 'decompress'],
     classifiers=[],
 )
